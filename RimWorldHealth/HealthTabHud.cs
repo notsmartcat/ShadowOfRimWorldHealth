6236c8c71b8ad46c2d0f9d4f1c42facb85be018f
notsmartcat <notsmartcat@gmail.com>
2026-01-29 01:24:23 +0000

    Initial commit

---
+++ RimWorldHealth/HealthTabHud.cs
@@ -0,0 +1,77 @@
+﻿using HUD;
+using RWCustom;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace ShadowOfRimWorldHealth;
+
+public class HealthTab : HudPart
+{
+    public HealthTab(HUD.HUD hud, AbstractCreature owner) : base(hud)
+    {
+        this.owner = owner;
+
+        InitiateSprites();
+    }
+
+    public Vector2 DrawPos(float timeStacker)
+    {
+        return new Vector2(hud.rainWorld.screenSize.x / 2, hud.rainWorld.screenSize.y / 2);
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    public void InitiateSprites()
+    {
+        sprites = new FSprite[1];
+
+        sprites[0] = new FSprite("pixel", true);
+
+        for (int k = 0; k < this.sprites.Length; k++)
+        {
+            this.hud.fContainers[1].AddChild(this.sprites[k]);
+        }
+    }
+
+    public override void Draw(float timeStacker)
+    {
+        base.Draw(timeStacker);
+
+        for (int i = 0; i < sprites.Length; i++)
+        {
+            sprites[i].isVisible = visible;
+
+            if (!visible)
+            {
+                return;
+            }
+
+            if (owner != null)
+            {
+                sprites[i].x = DrawPos(timeStacker).x;
+                sprites[i].y = DrawPos(timeStacker).y;
+                sprites[i].scale = 10;
+                sprites[i].color = Color.blue;
+            }
+        }
+    }
+
+    public override void ClearSprites()
+    {
+        base.ClearSprites();
+    }
+
+    public FLabel label;
+    public FSprite[] sprites;
+
+    public float actualWidth;
+    public Color currentColor;
+
+    public AbstractCreature owner;
+
+    public bool visible = false;
+}
---
+++ RimWorldHealth/ShadowOfRimWorldHealth.cs
@@ -0,0 +1,56 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using System;
+using UnityEngine;
+
+namespace RimWorldHealth;
+
+[BepInPlugin("notsmartcat.shadowofrimworldhealth", "RimWorld styled health", "1.0.0")]
+
+public class RimWorldHealth : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    public ShadowOfRimWorldHealth.HealthTab healthTab;
+
+    public void OnEnable()
+    {
+        try
+        {
+            Logger = base.Logger;
+
+            On.HUD.HUD.InitSinglePlayerHud += HUDInitSinglePlayerHud;
+
+            On.Player.Update += PlayerUpdate;
+        }
+        catch (Exception e) { Logger.LogError(e); }
+    }
+
+    private void HUDInitSinglePlayerHud(On.HUD.HUD.orig_InitSinglePlayerHud orig, HUD.HUD self, RoomCamera cam)
+    {
+        orig(self, cam);
+
+        healthTab = new ShadowOfRimWorldHealth.HealthTab(self, (self.owner as Creature).abstractCreature);
+
+        self.AddPart(healthTab);
+    }
+
+    void PlayerUpdate(On.Player.orig_Update orig, Player self, bool eu)
+    {
+        orig(self, eu);
+
+        if (Input.GetKeyDown("h"))
+        {
+            if (healthTab == null)
+            {
+                healthTab = new ShadowOfRimWorldHealth.HealthTab(self.room.game.cameras[0].hud, self.abstractCreature);
+
+                self.room.game.cameras[0].hud.AddPart(healthTab);
+            }
+
+            healthTab.visible = !healthTab.visible;
+
+            Debug.Log("HealthTab visibility is = " + healthTab.visible);
+        }
+    }
+}
